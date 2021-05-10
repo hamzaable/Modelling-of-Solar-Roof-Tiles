979b70fd3ca34b4daeccf0d00c4cd9a0b32a1835
Hamza <hamzaable@gmail.com>
2021-05-10 09:36:12 +0200

    Cooling Load Working

--- SDP.py
+++ SDP.py
@@ -69,6 +69,10 @@ sdp.init_sdp(ambient_temp=-4,
 df = []
 df1 = []
 
+dfMainElec = []
+dfSubElec = []
+dfThermalMain = []
+dfThermalSub = []
 # for i in pv_data.index[0:8760]:
 # Looping through weather data profile
 for i in pv_data.index[0:30]:
@@ -78,53 +82,20 @@ for i in pv_data.index[0:30]:
     ghi = pv_data.ghi[i]
     dni = pv_data.dni[i]
     dhi = pv_data.dhi[i]
+    Tamb = pv_data.temp_air[i]
     # finding different weather parameters using pvlib
     electrical_yield = Photovoltaic(latitude=latitude, longitude=longitude, altitude=altitude, timezone=timezone,
                                     time=pv_data.DateTimeIndex[i], dni=pv_data.dni[i], ghi=pv_data.ghi[i],
                                     dhi=pv_data.dhi[i], temp_amb=pv_data.temp_air[i], wind_amb=pv_data.wind_speed[i],
                                     pressure=pv_data.pressure[i])
     # Making an Array of results got from electrical_yield
-    df1 = [i, time, temp_amb, round(electrical_yield.annual_energy, 2), int(electrical_yield.effective_irradiance)]
+    dfSubElec = [i, time, temp_amb, round(electrical_yield.annual_energy, 2), int(electrical_yield.effective_irradiance)]
 
     # This point is important because here we can add cooling effect
+    E_sdp_1 = (0.93 * (dfSubElec[4] * 0.10)
+               - (dfSubElec[3])) / 0.10
 
-
-
-
-
-
-    #
-
-    # Append df1 to Main 2D Array df
-    df.append(df1)
-
-column_values = ["Index", "Time", "Tamb", "Power", "Effective_Irradiance"]
-# Assigning df all data to new varaible electrical data
-electrical_data = pd.DataFrame(data=df, columns=column_values)
-electrical_data.fillna(0) # fill empty rows with 0
-electrical_data.loc['Total'] = electrical_data.select_dtypes(np.number).sum() #  finding total number of rows
-pd.set_option('display.max_colwidth', 40)
-print(electrical_data)
-
-electrical_data.to_excel(r'Results1.xlsx')
-
-# ####
-# thermal Yeild
-# ###
-
-
-
-y = 0
-df = []
-df1 = []
-# for i in pv_data.index[0:8760]:
-
-for i in pv_data.index[0:30]:
-    time = pv_data.DateTimeIndex[i]
-    E_sdp = (0.93 * (electrical_data.Effective_Irradiance[i] * 0.10) - (electrical_data.Power[i])) / 0.10
-    Tamb = pv_data.temp_air[i]
-
-    if E_sdp == 0:
+    if E_sdp_1 == 0:
         # in deg celcius
         t_out_init = Tamb
 
@@ -137,7 +108,7 @@ for i in pv_data.index[0:30]:
     else:
         t_out_init, p_fan_init, m_out_init = sdp.calculate_sdp(
             ambient_temp=pv_data.temp_air[i],
-            absorption_incl=E_sdp,
+            absorption_incl=E_sdp_1,
             inlet_temp=pv_data.temp_air[i],
             mass_flow=1,
             print_res=False)
@@ -147,10 +118,11 @@ for i in pv_data.index[0:30]:
     m_out = m_out_init
     flux = round((m_out * (t_out - Tamb) / (num_sdp_series * num_sdp_parallel * 0.10)), 2)
 
-    elec_parameter = (house_data.elec_cons[i] + p_fan) < (electrical_data.Power[i] * num_sdp_parallel * num_sdp_series)
+    elec_parameter = (house_data.elec_cons[i] + p_fan) \
+                     < (dfSubElec[3] * num_sdp_parallel * num_sdp_series)
     thermal_parameter = (house_data.thermal_cons[i] < flux)
 
-    if E_sdp == 0:
+    if E_sdp_1 == 0:
         status = "System Off"
 
     elif (elec_parameter is True) and (thermal_parameter is True):
@@ -187,20 +159,30 @@ for i in pv_data.index[0:30]:
     else:
         status = "System Off"
 
-    df1 = [i, time, Tamb, round(E_sdp, 2), t_out, p_fan, m_out, flux, status, elec_parameter, thermal_parameter]
-    df.append(df1)
-print(df)
+    dfThermalSub = [i, time, Tamb, round(E_sdp_1, 2), t_out, p_fan, m_out, flux, status, elec_parameter, thermal_parameter]
+    dfThermalMain.append(dfThermalSub)
+
+    dfMainElec.append(dfSubElec)
+
+column_values_elec = ["Index", "Time", "Tamb", "Power", "Effective_Irradiance"]
+# Assigning df all data to new varaible electrical data
+electrical_data = pd.DataFrame(data=dfMainElec, columns=column_values_elec)
+electrical_data.fillna(0) # fill empty rows with 0
+electrical_data.loc['Total'] = electrical_data.select_dtypes(np.number).sum() #  finding total number of rows
+pd.set_option('display.max_colwidth', 40)
+print(electrical_data)
+electrical_data.to_excel(r'Results1.xlsx')
 
 column_values = ["Index", "Time", "Tamb", "E_sdp_eff", "T_out", "P_fan", "M_out", "HeatFlux", "status",
                  "Elec_demand_met", "Heat_demand_met"]
-thermal_data = pd.DataFrame(data=df, columns=column_values)
+thermal_data = pd.DataFrame(data=dfThermalMain, columns=column_values)
 thermal_data.loc['Total'] = thermal_data.select_dtypes(np.number).sum()
 pd.set_option('display.max_colwidth', 8)
 print(thermal_data)
 
-# Efficiency wrt effective E sdp for solar
 Efficiency = thermal_data.loc["Total", "HeatFlux"] / thermal_data.loc["Total", "E_sdp_eff"]
 print("Efficiency wrt Effective Irradiance:", round(Efficiency * 100, 2), "%")
 
 complete_data = pd.merge(electrical_data, thermal_data)
 complete_data.to_excel(r'Result2.xlsx')
+

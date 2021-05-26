53408ccb2997922deefb307de830819ab647bb6a
Hamza <hamzaable@gmail.com>
2021-05-26 22:54:16 +0200

    Updated Cooling Effect

--- SDP.py
+++ SDP.py
@@ -21,14 +21,14 @@ timezone = 'Etc/GMT+2'
 
 "______Photovoltaic Parameters_________"
 
-albedo = 0.20               # Ground reflection albedo factor 0.20 (Beton) --> K. Mertens- Photovoltaik S.52
-a_r = 0.14                  # Spectral Corrections factor for different module glasses
-irrad_model= 'haydavies'    # Model for Irradiation calculation. Choose from: 'isotropic', 'klucher', 'haydavies', 'reindl', 'king', 'perez'
-m_azimut = 180              # Module Azimut (Ausrichtung) [°]dwd_data = pd.read_excel(r'704EEE00.xlsx')  # Hourly Weather Data (DNI , GHI , DHI , temp_air , wind speed and pressure)
-m_tilt = 45     	        # Module tilt (Neigung) [°]
+albedo = 0.20  # Ground reflection albedo factor 0.20 (Beton) --> K. Mertens- Photovoltaik S.52
+a_r = 0.14  # Spectral Corrections factor for different module glasses
+irrad_model = 'haydavies'  # Model for Irradiation calculation. Choose from: 'isotropic', 'klucher', 'haydavies', 'reindl', 'king', 'perez'
+m_azimut = 180  # Module Azimut (Ausrichtung) [°]dwd_data = pd.read_excel(r'704EEE00.xlsx')  # Hourly Weather Data (DNI , GHI , DHI , temp_air , wind speed and pressure)
+m_tilt = 45  # Module tilt (Neigung) [°]
 
-#======Module Parameters=======================================================
-	# #ar=0.14
+# ======Module Parameters=======================================================
+# #ar=0.14
 module = {"Vintage": 2020, "Area": 0.1, "Material": "mc-Si", "celltype": "monoSi", "Cells_in_Series": 8,
           "Parallel_Strings": 2, "Isco": 3.5, "Voco": 5.36, "Impo": 3.3, "Vmpo": 4.568,
           "Aisc": 0.0010, "Bvoco": -0.0158, "Bvmpo": -0.01608, "gamma_pmp": -0.3792,
@@ -54,8 +54,7 @@ pv_data["temp_air"] = dwd_data.temp
 pv_data["wind_speed"] = dwd_data.wind_speed
 pv_data["pressure"] = dwd_data.pr
 
-
-house_data_read = pd.read_excel(r'house_demand.xlsx') # Hourly house demand (elec_cons , thermal_cons)
+house_data_read = pd.read_excel(r'house_demand.xlsx')  # Hourly house demand (elec_cons , thermal_cons)
 house_data = pd.DataFrame(index=house_data_read.index, columns=["elec_cons", "thermal_cons"])
 
 # Assign Headers to pv_data Dataframe
@@ -65,8 +64,8 @@ house_data["elec_cons"] = house_data_read.elec_cons
 house_data["thermal_cons"] = house_data_read.thermal_cons
 
 "______TESPy Model Parameters_________"
-num_sdp_series = 12     #Changed from 12 to 2 for test purpose
-num_sdp_parallel = 38   ##Changed from 38 to 1 for test purpose
+num_sdp_series = 12  # Changed from 12 to 2 for test purpose
+num_sdp_parallel = 38  ##Changed from 38 to 1 for test purpose
 
 #####
 # Thermal initialization
@@ -81,7 +80,6 @@ sdp.init_sdp(ambient_temp=-4,
              # zeta=4e6,
              m_loss=0.001,
              print_res=False)
-
 
 "_____________Calculations____________"
 
@@ -109,24 +107,27 @@ for i in pv_data.index[8:30]:
     newResidue = 0
     oldResidue = 0
     # finding different weather parameters using pvlib
-                                    
-    electrical_yield= Photovoltaic(latitude=latitude, longitude=longitude, altitude=altitude, timezone=timezone,
-                                   m_azimut=m_azimut, m_tilt=m_tilt, module_number=num_sdp_series*num_sdp_parallel,
-                                   time=pv_data.DateTimeIndex[i], dni=pv_data.dni[i], ghi=pv_data.ghi[i], dhi=pv_data.dhi[i],
-                                   albedo=albedo, a_r=a_r, irrad_model=irrad_model, module=module,
-                                   temp_amb=pv_data.temp_air[i], wind_amb=pv_data.wind_speed[i],pressure=pv_data.pressure[i])
+
+    electrical_yield = Photovoltaic(latitude=latitude, longitude=longitude, altitude=altitude, timezone=timezone,
+                                    m_azimut=m_azimut, m_tilt=m_tilt, module_number=num_sdp_series * num_sdp_parallel,
+                                    time=pv_data.DateTimeIndex[i], dni=pv_data.dni[i], ghi=pv_data.ghi[i],
+                                    dhi=pv_data.dhi[i],
+                                    albedo=albedo, a_r=a_r, irrad_model=irrad_model, module=module,
+                                    temp_amb=pv_data.temp_air[i], wind_amb=pv_data.wind_speed[i],
+                                    pressure=pv_data.pressure[i],temp_avg=pv_data.temp_air[i])
 
     # Making an Array of results got from electrical_yield
-    dfSubElec = [i, time, temp_amb, round(electrical_yield.annual_energy, 2), int(electrical_yield.effective_irradiance)]
+    dfSubElec = [i, time, temp_amb, round(electrical_yield.annual_energy, 2),
+                 int(electrical_yield.effective_irradiance)]
     # step 1
 
     P_MP = dfSubElec[3] / (num_sdp_series * num_sdp_parallel)
 
-    effective_Iradiance =dfSubElec[4]
+    effective_Iradiance = dfSubElec[4]
 
     # This point is important because here we can add cooling effect
     #  Step 2
-    E_sdp_1 = (0.93 * (effective_Iradiance * 0.10) - (P_MP)) / 0.10
+    E_sdp_1 = (0.93 * (effective_Iradiance * module['Area']) - (P_MP)) / module['Area']
 
     if E_sdp_1 == 0:
         # in deg Celsius
@@ -150,36 +151,39 @@ for i in pv_data.index[8:30]:
     t_out = t_out_init
     # Step 4
     T_PV_Temp_Model = float(electrical_yield.tcell)
-    t_avg = (T_PV_Temp_Model + t_out)/2
-    # Step 4.5 finding residue
-    newResidue = t_avg - t_out
-    # Step 5 calculating Power again based on new temp
+    t_avg = (T_PV_Temp_Model + t_out) / 2
+    # Step 5 residue to perform ite
+    t_avg_new = t_avg
+    newResidue = t_avg_new - t_out
+    #
+    while newResidue > 0.2:
+        electrical_yield_new = Photovoltaic(latitude=latitude, longitude=longitude, altitude=altitude, timezone=timezone,
+                                        m_azimut=m_azimut, m_tilt=m_tilt,
+                                        module_number=num_sdp_series * num_sdp_parallel,
+                                        time=pv_data.DateTimeIndex[i], dni=pv_data.dni[i], ghi=pv_data.ghi[i],
+                                        dhi=pv_data.dhi[i],
+                                        albedo=albedo, a_r=a_r, irrad_model=irrad_model, module=module,
+                                        temp_amb=pv_data.temp_air[i], wind_amb=pv_data.wind_speed[i],
+                                        pressure=pv_data.pressure[i],temp_avg=t_avg_new)
 
-    while newResidue > 0.5:
-        print ("Hamza")
-        x= 7
-        x +=1
-        if x==8:
+        dfSubElec_New = [i, time, temp_amb, round(electrical_yield_new.annual_energy, 2),
+                     int(electrical_yield_new.effective_irradiance)]
+        t_avg_old = t_avg_new
+        t_avg_new = (float(electrical_yield_new.tcell_new) + t_out) / 2
+        newResidue = t_avg_new - t_avg_old
+        if newResidue  <0.2:
             break
-    electrical_yield_new = Photovoltaic(latitude=latitude, longitude=longitude, altitude=altitude, timezone=timezone,
-                                    m_azimut=m_azimut, m_tilt=m_tilt, module_number=num_sdp_series * num_sdp_parallel,
-                                    time=pv_data.DateTimeIndex[i], dni=pv_data.dni[i], ghi=pv_data.ghi[i],
-                                    dhi=pv_data.dhi[i],
-                                    albedo=albedo, a_r=a_r, irrad_model=irrad_model, module=module,
-                                    temp_amb=t_avg, wind_amb=pv_data.wind_speed[i],
-                                    pressure=pv_data.pressure[i])
 
-    dfSubElec_New = [i, time, temp_amb, round(electrical_yield_new.annual_energy, 2),
-                 int(electrical_yield_new.effective_irradiance)]
 
-    P_MP = dfSubElec_New[3]/ (num_sdp_series*num_sdp_parallel)
+    P_MP_New = dfSubElec_New[3] / (num_sdp_series * num_sdp_parallel)
 
     # Step 6 New E_SDP
-    E_sdp_1 = (0.93 * (effective_Iradiance * 0.10) - (P_MP)) / 0.10
+    E_sdp_1 = (0.93 * (effective_Iradiance * module['Area']) - (P_MP)) / module['Area']
     # End of all steps
 
     p_fan = p_fan_init
     m_out = m_out_init
+
     flux = round((m_out * (t_out - Tamb) / (num_sdp_series * num_sdp_parallel * 0.10)), 2)
 
     elec_parameter = (house_data.elec_cons[i] + p_fan) \
@@ -201,7 +205,8 @@ for i in pv_data.index[8:30]:
     else:
         status = "System Off"
 
-    dfThermalSub = [i, time, Tamb, round(E_sdp_1, 2), t_out, p_fan, m_out, flux, status, elec_parameter, thermal_parameter]
+    dfThermalSub = [i, time, Tamb, round(E_sdp_1, 2), t_out, p_fan, m_out, flux, status, elec_parameter,
+                    thermal_parameter]
     dfThermalMain.append(dfThermalSub)
 
     dfMainElec.append(dfSubElec_New)
@@ -209,8 +214,8 @@ for i in pv_data.index[8:30]:
 column_values_elec = ["Index", "Time", "Tamb [°C]", "Power [W]", "Effective Irradiance [W/m^2]"]
 # Assigning df all data to new varaible electrical data
 electrical_data = pd.DataFrame(data=dfMainElec, columns=column_values_elec)
-electrical_data.fillna(0) # fill empty rows with 0
-electrical_data.loc['Total'] = electrical_data.select_dtypes(np.number).sum() #  finding total number of rows
+electrical_data.fillna(0)  # fill empty rows with 0
+electrical_data.loc['Total'] = electrical_data.select_dtypes(np.number).sum()  # finding total number of rows
 pd.set_option('display.max_colwidth', 40)
 print(electrical_data)
 
@@ -229,4 +234,3 @@ print("Efficiency wrt Effective Irradiance:", round(Efficiency * 100, 2), "%")
 
 complete_data = pd.merge(electrical_data, thermal_data)
 complete_data.to_excel(r'Result2.xlsx')
-

c69eef587baa5d3bf3d62017c9b93ffd851abaf7
Hamza <hamzarsaleemi@gmail.com>
2022-01-06 19:14:08 +0100

    New Results

--- SDP.py
+++ SDP.py
@@ -215,7 +215,7 @@ totalPowerDiff = 0
 countNonZero = 0
 
 #for i in tqdm(pv_data.index[8:10]):
-for i in tqdm(pv_data.index[0:24]):   # One Year Sim.: [0:8759]
+for i in tqdm(pv_data.index[0:8759]):   # One Year Sim.: [0:8759]
 
 
     "_______Looping through excel rows_______"

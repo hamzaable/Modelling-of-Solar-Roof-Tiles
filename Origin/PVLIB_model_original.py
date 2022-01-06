a02fc1ea35799c41b72a3e74b15480dfffaa0e18
Marius Bartkowski <marius.bartkowski@smail.th-koeln.de>
2022-01-06 11:56:59 +0100

    Update PVLIB_model_original.py
    - updated energy yield calcualtion

--- Origin/PVLIB_model_original.py
+++ Origin/PVLIB_model_original.py
@@ -99,14 +99,93 @@ class Photovoltaic():
             self.dc = pvlib.pvsystem.sapm(self.effective_irradiance, self.tcell, module)
             self.annual_energy = self.dc['p_mp'].sum()
             
-               
-# column_values = ["Angle","Yield"]
-# electrical_data = pd.DataFrame(data=energies, columns = column_values)
-# electrical_data.fillna(0)
-# electrical_data.loc['Total'] = electrical_data.select_dtypes(np.number).sum()
-# pd.set_option('display.max_colwidth', 40)
-# print(electrical_data)
-# electrical_data.to_excel(r'D:\REM\SEM III\Project 2\03.06.2021\Results2.xlsx')
+            #Estimates parameters for the CEC single diode model (SDM) using the SAM SDK
+            #For Details on the single module specs see module definition in SDP.py 
+            self.mp_fit_cec_sam = pvlib.ivtools.sdm.fit_cec_sam(
+                self.module['celltype'],                                        
+                v_mp=self.module['Vmpo'], 
+                i_mp=self.module['Impo'],                                       
+                v_oc=self.module['Voco'], 
+                i_sc=self.module['Isco'],
+                alpha_sc=self.module['Aisc'], 
+                beta_voc=self.module['Bvoco'], 
+                gamma_pmp=self.module['gamma_pmp'], 
+                cells_in_series=self.module['Cells_in_Series'],
+                temp_ref=25)
+            
+                                
+            #Calculating parameters for solving the single diod model by De Soto et. al model
+            self.single_diod_parameters = pvlib.pvsystem.calcparams_desoto(
+                self.effective_irradiance, 
+                self.tcell,                                                     # cell temperature 
+                alpha_sc=self.module['Aisc'],                                   # See SDP.py
+                a_ref=self.mp_fit_cec_sam[4],                                   # The product of the usual diode ideality factor n (unitless), number of cells in series, and cell thermal voltage at reference conditions [V]
+                I_L_ref=self.mp_fit_cec_sam[0],                                 # The light-generated current (or photocurrent) at reference conditions [A]
+                I_o_ref=self.mp_fit_cec_sam[1],                                 # The dark or diode reverse saturation current at reference conditions [A]
+                R_sh_ref=self.mp_fit_cec_sam[3],                                # The shunt resistance at reference conditions, in ohms.
+                R_s=self.mp_fit_cec_sam[2],                                     # The series resistance at reference conditions, in ohms.
+                EgRef=1.121,                                                    # The energy bandgap at reference temperature in units of eV. 1.121 eV for crystalline silicon. EgRef must be >0. For parameters from the SAM CEC module database, EgRef=1.121 is implicit for all cell types in the parameter estimation algorithm used by NREL
+                dEgdT=- 0.0002677,                                              # The temperature dependence of the energy bandgap at reference conditions in units of 1/K. May be either a scalar value (e.g. -0.0002677 as in 1) or a DataFrame (this may be useful if dEgdT is a modeled as a function of temperature). For parameters from the SAM CEC module database, dEgdT=-0.0002677 is implicit for all cell types in the parameter estimation algorithm used by NREL.
+                irrad_ref=1000,                                                 # Reference irradiance in W/m^2
+                temp_ref=25)                                                    # Reference cell temperature in C
+                        
+            
+            #Solving the single-diode equation to obtain a photovoltaic IV curve. Important is the output p_mp
+            self.single_diod_IV = pvlib.pvsystem.singlediode(
+                photocurrent=self.single_diod_parameters[0],                    # Light-generated current IL (photocurrent)
+                saturation_current=self.single_diod_parameters[1],              # Series resistance Rs under desired IV curve conditions
+                resistance_series=self.single_diod_parameters[2],               # Series resistance Rs under desired IV curve conditions
+                resistance_shunt=self.single_diod_parameters[3],                # Shunt resistance Rsh under desired IV curve conditions.
+                nNsVth=self.single_diod_parameters[4],                          # The product of three components: 1) the usual diode ideality factor n, 2) the number of cells in series Ns, and 3) the cell thermal voltage Vth. 
+                ivcurve_pnts=None,                                              # Number of points in the desired IV curve. If None or 0, no points on the IV curves will be produced.
+                method='lambertw')                                              # Determines the method used to calculate points on the IV curve. The options are 'lambertw', 'newton', or 'brentq'
+            
+            # Storing module Power to pass it on to the Excel Sheet 
+            # Power output calculation for mutliple modules (DC)
+            self.annual_energy = self.module_number*self.single_diod_IV['p_mp'].sum() #in Watt hours, because p_mp is given in Watt
+            
+            # Power output calculation for mutliple modules (AC) with PVWatts Inverter Method (NREL)
+            """
+                
+                NREL’s PVWatts inverter model.
+                The PVWatts inverter model [1] calculates inverter efficiency η as a function of input DC power
+                [1] A. P. Dobos, “PVWatts Version 5 Manual,” http://pvwatts.nrel.gov/downloads/pvwattsv5.pdf (2014).
+                
+                For the estimation of the fitting inverter see Project2_further-calculations.xlsx Sheet 'Inverter DC/AC'
+                Rated power of System = 12 * 16 * 15 W = 2.880 kW --> pdc0 assumed to be 3.5 kW
+            
+                Parameters:
+                -----------
+                
+                    pdc : numeric
+                        DC power. Same unit as pdc0.
+                        
+                    pdc0 : numeric
+                        DC input limit of the inverter. Same unit as pdc.
+                        
+                    eta_inv_nom : numeric
+                        Nominal inverter efficiency. [unitless]. Default 0.96
+                        
+                    eta_inv_ref : numeric
+                        Reference inverter efficiency. PVWatts defines it to be 0.9637 and is included here for flexibility. [unitless]
+                        The reference inverter effiency_inv_ref is taken from the CEC data for the actual most typical inverter, which is 0.9637 (Pac0/Pdc0)
+                    
+                   
+                Outputs:
+                --------
+                
+                    power_ac : numeric
+                        AC power. Same unit as pdc0.
+                                            
+            """         
+            
+            self.power_ac = pvlib.inverter.pvwatts(
+                pdc=self.annual_energy,                                         
+                pdc0= 6000,                                                      # Used Inverter is SMA Sunny Boy 1.5            
+                eta_inv_nom=0.961,                                               # Maximum efficiency 97,2 % / Euro-eta 96,1 % 
+                eta_inv_ref=0.9637)                                              # based on CEC Data
+                
+                   
 
 
 

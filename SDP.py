1a4dd0d2f9edc59e1daabf3f99e329ebbecf2418
MariusBartkowski <marius.bartkowski@smail.th-koeln.de>
2022-01-28 12:08:43 +0100

    Old Modal

---
+++ SDP.py
@@ -0,0 +1,201 @@
+# -*- coding: utf-8 -*-
+#  Test cooling effect
+import pandas as pd
+import numpy as np
+
+##########
+from PVLIB_model import Photovoltaic
+from TESPy_model import SDP_sucking
+
+##########
+start = "01-01-{} 00:00".format(str(2019))
+end = "31-12-{} 23:00".format(str(2019))
+naive_times = pd.date_range(start=start, end=end, freq='1h')
+
+latitude = 50.934055
+longitude = 6.990349
+name = 'Cologne'
+altitude = 121
+timezone = 'Etc/GMT+2'
+
+
+
+albedo = 0.20                                                                   # Ground reflection albedo factor 0.20 (Beton) --> K. Mertens- Photovoltaik S.52
+a_r = 0.14                                                                      # Spectral Corrections factor for different module glasses
+irrad_model = 'haydavies'                                                       # Model for Irradiation calculation. Choose from: 'isotropic', 'klucher', 'haydavies', 'reindl', 'king', 'perez'
+P_PV_STC = 14.4                                                                 # rated power from TüV Calibration report under STC Conditions at 25°C & 1000 W/m^2
+m_azimut = 180                                                                  # Module Azimut (Ausrichtung) [°]
+m_tilt = 39                                                                     # Module tilt (Neigung) [°]
+
+
+
+dwd_data = pd.read_excel(r'704EEE00.xlsx')  # Hourly Weather Data (DNI , GHI , DHI , temp_air , wind speed and pressure)
+
+pv_data = pd.DataFrame(index=dwd_data.index, columns=["dni", "ghi",
+                                                      "dhi",
+                                                      "temp_air",
+                                                      "wind_speed", "pressure"])
+# Assign Headers to pv_data Dataframe
+
+pv_data["DateTimeIndex"] = dwd_data.date
+pv_data["DateTimeIndex"] = pd.to_datetime(pv_data["DateTimeIndex"])
+pv_data["dni"] = dwd_data.irradiance_dir
+pv_data["dhi"] = dwd_data.irradiance_diff
+pv_data["ghi"] = dwd_data.poa_global
+pv_data["temp_air"] = dwd_data.temp
+pv_data["wind_speed"] = dwd_data.wind_speed
+pv_data["pressure"] = dwd_data.pr
+
+
+house_data_read = pd.read_excel(r'house_demand.xlsx') # Hourly house demand (elec_cons , thermal_cons)
+house_data = pd.DataFrame(index=house_data_read.index, columns=["elec_cons", "thermal_cons"])
+
+# Assign Headers to pv_data Dataframe
+house_data["DateTimeIndex"] = house_data_read.date
+house_data["DateTimeIndex"] = pd.to_datetime(house_data["DateTimeIndex"])
+house_data["elec_cons"] = house_data_read.elec_cons
+house_data["thermal_cons"] = house_data_read.thermal_cons
+
+num_sdp_series = 96
+num_sdp_parallel = 2
+
+#####
+# Thermal initialization
+#####
+sdp = SDP_sucking(sdp_in_parallel=num_sdp_parallel,
+                  sdp_in_series=num_sdp_series)
+
+sdp.init_sdp(ambient_temp=-4,
+             absorption_incl=300,
+             inlet_temp=-4,
+             mass_flow=1,
+             # zeta=4e6,
+             m_loss=0.001,
+             print_res=False)
+
+######
+# Electrical Yeild
+######
+
+dfMainElec = []
+dfSubElec = []
+dfSubElec_New = []
+dfThermalMain = []
+dfThermalSub = []
+# for i in pv_data.index[0:8760]:
+# Looping through weather data profile
+for i in pv_data.index[8:30]:
+    print('Loop number : ' + str(i))
+    time = pv_data.DateTimeIndex[i]
+    temp_amb = pv_data.temp_air[i]
+    wind_amb = pv_data.wind_speed[i]
+    ghi = pv_data.ghi[i]
+    dni = pv_data.dni[i]
+    dhi = pv_data.dhi[i]
+    Tamb = pv_data.temp_air[i]
+    # finding different weather parameters using pvlib
+    electrical_yield = Photovoltaic(latitude=latitude, longitude=longitude, altitude=altitude, timezone=timezone,
+                                    time=pv_data.DateTimeIndex[i], dni=pv_data.dni[i], ghi=pv_data.ghi[i],
+                                    dhi=pv_data.dhi[i], temp_amb=pv_data.temp_air[i], wind_amb=pv_data.wind_speed[i],
+                                    pressure=pv_data.pressure[i])
+    # Part of Step 1
+    T_PV_Temp_Model = float(electrical_yield.tcell)
+
+    # Making an Array of results got from electrical_yield
+    dfSubElec = [i, time, temp_amb, round(electrical_yield.annual_energy, 2), int(electrical_yield.effective_irradiance)]
+    # step 1
+    P_MP = dfSubElec[3]
+    effective_Iradiance =dfSubElec[4]
+
+    # This point is important because here we can add cooling effect
+    #  Step 2
+    E_sdp_1 = (0.93 * (effective_Iradiance * 0.10)  - (P_MP)) / 0.10
+
+
+    if E_sdp_1 == 0:
+        # in deg Celsius
+        t_out_init = Tamb
+
+        # Watt
+        p_fan_init = 0
+
+        # kg/sec
+        m_out_init = 0
+
+    else:
+        t_out_init, p_fan_init, m_out_init = sdp.calculate_sdp(
+            ambient_temp=pv_data.temp_air[i],
+            absorption_incl=E_sdp_1,
+            inlet_temp=pv_data.temp_air[i],
+            mass_flow=1,
+            print_res=False)
+
+    # Step 3
+    t_out = t_out_init
+    # Step 4
+    t_avg = (T_PV_Temp_Model + t_out)/2
+    # Step 5 calculating Power again based on new temp
+    electrical_yield_new = Photovoltaic(latitude=latitude, longitude=longitude, altitude=altitude, timezone=timezone,
+                                    time=pv_data.DateTimeIndex[i], dni=pv_data.dni[i], ghi=pv_data.ghi[i],
+                                    dhi=pv_data.dhi[i], temp_amb=t_avg, wind_amb=pv_data.wind_speed[i],
+                                    pressure=pv_data.pressure[i])
+    dfSubElec_New = [i, time, temp_amb, round(electrical_yield_new.annual_energy, 2), int(electrical_yield_new.effective_irradiance)]
+    P_MP = dfSubElec_New[3]
+
+    # Step 6 New E_SDP
+    E_sdp_1 = (0.93 * (effective_Iradiance * 0.10) - (P_MP)) / 0.10
+    # End of all steps
+
+    p_fan = p_fan_init
+    m_out = m_out_init
+    flux = round((m_out * (t_out - Tamb) / (num_sdp_series * num_sdp_parallel * 0.10)), 2)
+
+    elec_parameter = (house_data.elec_cons[i] + p_fan) \
+                     < (P_MP * num_sdp_parallel * num_sdp_series)
+    thermal_parameter = (house_data.thermal_cons[i] < flux)
+
+    if E_sdp_1 == 0:
+        status = "System Off"
+
+    elif (elec_parameter is True) and (thermal_parameter is True):
+        status = "Good"
+
+    elif (elec_parameter is True) and (thermal_parameter is False):
+        status = "Increase Thermal Production (increase fan_power)"
+
+
+    elif (elec_parameter is False) and (thermal_parameter is True):
+        status = "Decrease Thermal Production (reduce fan_power)"
+
+
+    else:
+        status = "System Off"
+
+    dfThermalSub = [i, time, Tamb, round(E_sdp_1, 2), t_out, p_fan, m_out, flux, status, elec_parameter, thermal_parameter]
+    dfThermalMain.append(dfThermalSub)
+
+    dfMainElec.append(dfSubElec_New)
+
+column_values_elec = ["Index", "Time", "Tamb", "Power", "Effective_Irradiance"]
+# Assigning df all data to new varaible electrical data
+electrical_data = pd.DataFrame(data=dfMainElec, columns=column_values_elec)
+electrical_data.fillna(0) # fill empty rows with 0
+electrical_data.loc['Total'] = electrical_data.select_dtypes(np.number).sum() #  finding total number of rows
+pd.set_option('display.max_colwidth', 40)
+print(electrical_data)
+electrical_data.to_excel(r'Results1.xlsx')
+
+
+column_values = ["Index", "Time", "Tamb", "E_sdp_eff", "T_out", "P_fan", "M_out", "HeatFlux", "status",
+                 "Elec_demand_met", "Heat_demand_met"]
+thermal_data = pd.DataFrame(data=dfThermalMain, columns=column_values)
+thermal_data.loc['Total'] = thermal_data.select_dtypes(np.number).sum()
+pd.set_option('display.max_colwidth', 8)
+print(thermal_data)
+
+Efficiency = thermal_data.loc["Total", "HeatFlux"] / thermal_data.loc["Total", "E_sdp_eff"]
+print("Efficiency wrt Effective Irradiance:", round(Efficiency * 100, 2), "%")
+
+complete_data = pd.merge(electrical_data, thermal_data)
+complete_data.to_excel(r'CompleteResults.xlsx')
+
---
+++ TESPy_model.py
@@ -0,0 +1,476 @@
+# -*- coding: utf-8 -*-
+
+
+from tespy.networks import Network
+from tespy.components import (Sink, Source, Valve, Merge,
+                              Subsystem, Compressor,
+                              SolarCollector)
+from tespy.connections import Connection
+
+from tespy.tools.logger import logging
+# from tespy.tools import logger
+from tespy.tools.helpers import TESPyComponentError
+import math
+
+Temp_Units = {
+    'C': [273.15, 1], 'F': [459.67, 5 / 9], 'K': [0, 1],
+    'R': [0, 5 / 9]
+}
+
+
+class sdp_subsys(Subsystem):
+    """
+    subsystem definition for the sdp.
+
+    It consists of the solar-roof tiles,
+    connected in series and also includes a gap between the tiles, where air
+    is drawn in. This model supposes, that the air is drawn from a ventilator
+    at the end of the roof. The ventilator is not included in the subsystem.
+
+    For connecting in series and parallel
+
+    """
+
+    def __init__(self, label,
+                 num_sdp_series,
+                 lkf_lin_sdp,
+                 lkf_quad_sdp,
+                 A_sdp,
+                 Tamb,
+                 E_sdp,
+                 ks_sdp,
+                 d_sdp,
+                 l_sdp,
+                 p_amb,
+                 m_loss=None,
+                 zeta_sdp=None):
+
+        if not isinstance(label, str):
+            msg = 'Subsystem label must be of type str!'
+            logging.error(msg)
+            raise ValueError(msg)
+
+        elif len([x for x in [';', ', ', '.'] if x in label]) > 0:
+            msg = 'Can\'t use ' + str([';', ', ', '.']) + ' in label.'
+            logging.error(msg)
+            raise ValueError(msg)
+        else:
+            self.label = label
+
+        if num_sdp_series <= 1:
+            raise TESPyComponentError('Minimum number of sdp in series is 2.')
+        else:
+            self.num_sdp_series = num_sdp_series
+        if m_loss is None and zeta_sdp is None:
+            raise TESPyComponentError('Please provide either m_loss or zeta_sdp.')
+
+        self.lkf_lin_sdp = lkf_lin_sdp
+        self.lkf_quad_sdp = lkf_quad_sdp
+        self.A_sdp = A_sdp
+        self.Tamb = Tamb
+        self.E_sdp = E_sdp
+        self.ks_sdp = ks_sdp
+        self.d_sdp = d_sdp
+        self.l_sdp = l_sdp
+        self.m_loss = m_loss
+        self.zeta_sdp = zeta_sdp
+        self.p_amb = p_amb
+
+        self.comps = {}
+        self.conns = {}
+        self._create_comps()
+        self._create_conns()
+
+    def _create_comps(self):
+
+        for i in range(self.num_sdp_series):
+            j = str(i)
+
+            self.comps['merge_' + j] = Merge(label=self.label +
+                                                   '_merge_' + j)
+
+            self.comps['sdp_' + j] = SolarCollector(label=self.label +
+                                                          '_sdp_' + j,
+                                                    lkf_lin=self.lkf_lin_sdp,
+                                                    lkf_quad=self.lkf_quad_sdp,
+                                                    A=self.A_sdp,
+                                                    E=self.E_sdp,
+                                                    D=self.d_sdp,
+                                                    L=self.l_sdp,
+                                                    Tamb=self.Tamb,
+                                                    ks=self.ks_sdp,
+                                                    eta_opt=1)
+
+            self.comps['Valve_' + j] = Valve(label=self.label + '_Valve_' + j)
+
+            if self.zeta_sdp is not None:
+                self.comps['Valve_' + j].set_attr(zeta=self.zeta_sdp)
+            self.comps['Source' + j] = Source(label=self.label + '_Source_' + j)
+
+    def _create_conns(self):
+
+        for i in range(self.num_sdp_series):
+            j = str(i)
+
+            if i > 0:
+                self.conns['mesd_' + j] = Connection(self.comps['merge_' +
+                                                                str(i - 1)],
+                                                     'out1',
+                                                     self.comps['sdp_' + j],
+                                                     'in1')
+
+            self.conns['sdme_' + j] = Connection(self.comps['sdp_' + j],
+                                                 'out1',
+                                                 self.comps['merge_' + j],
+                                                 'in1')
+            if self.m_loss is not None:
+                self.conns['sova_' + j] = Connection(
+                    self.comps['Source' + j],
+                    'out1',
+                    self.comps['Valve_' + j],
+                    'in1',
+                    p=self.p_amb,
+                    T=self.Tamb,
+                    fluid={'air': 1},
+                    m=self.m_loss)
+            else:
+                self.conns['sova_' + j] = Connection(
+                    self.comps['Source' + j],
+                    'out1',
+                    self.comps['Valve_' + j],
+                    'in1',
+                    p=self.p_amb,
+                    T=self.Tamb,
+                    fluid={'air': 1},
+                    m0=0.0001)
+            self.conns['vame_' + j] = Connection(
+                self.comps['Valve_' + j],
+                'out1',
+                self.comps['merge_' + j],
+                'in2',
+                p0=1.01)
+
+
+class SDP_sucking():
+    """
+    Model of the SDP
+    """
+
+    def __init__(self,
+                 sdp_in_series,
+                 sdp_in_parallel,
+                 loss_key_figures_lin=34,
+                 loss_key_figures_quad=0.03,
+                 width_sdp=0.244,
+                 thickness_sdp=0.02,
+                 l_sdp=0.3275,
+                 ks=0.0001,
+                 zeta=None):
+
+        """
+        Simulates the solar roof tile with a fan at the back of it, which is
+        "sucking" air from the sdps and into the air-Source heat-pump.
+        This TESPy model simulates each sdp in a series of sdps and thus
+        includes effects of leakage and uneven temperature distribution.
+
+        Parameters:
+        -----------
+
+            num_sdp_series : int
+                number of SDPs connected in series for the model
+
+            num_sdp_parallel : int
+                number of SDPs connected in parallel for the model
+
+            loss_key_figures_lin : float
+                the losses of one sdp, linearly dependent on the temperature
+                difference
+
+            loss_key_figures_quad : float
+                the losses of one sdp, quadratically dependent on the
+                temperature difference
+
+            width_sdp : float
+                the width of the sdp in m
+
+            thickness_sdp : float
+                the thickness of the sdp in m
+
+            l_sdp : float
+                the length (or height) of the sdp in m
+        """
+
+        # -------------------------------------------------------------------
+        # For the TESPy model...
+        self.num_sdp_series = sdp_in_series
+        self.num_sdp_parallel = sdp_in_parallel
+
+        # loss figures of the sdp
+        self.loss_key_figures_lin = loss_key_figures_lin
+        self.loss_key_figures_quad = loss_key_figures_quad
+
+        # width of the sdp in m
+        self.width_sdp = width_sdp
+
+        # thickness of the sdp in m
+        self.thickness_sdp = thickness_sdp
+
+        # summarized length of the sdp:
+        self.l_sdp = l_sdp  # height/length of the sdp in m
+
+        # equivalent diameter of the sdp to calculate the pressure loss via the
+        # ks value
+        self.d_sdp = math.sqrt(thickness_sdp * width_sdp * 2 / math.pi)
+
+        # the surface area of the sdp in m²
+        self.surface_area_sdp = width_sdp * l_sdp
+
+        # zeta value for the leakage in each sdp
+        self.zeta = zeta
+
+        # ks value for the pressure loss inside each sdp
+        self.ks = ks
+
+    # %% initialisation of the sdp
+
+    def init_sdp(self,
+                 ambient_temp,
+                 absorption_incl,
+                 inlet_temp,
+                 mass_flow,
+                 p_amb=1.01325,
+                 m_loss=None,
+                 zeta=None,
+                 print_res=True):
+        """
+        Initializing the TESPy simulation of the SDP. This initialization is
+        stored in the folder "sdp", which is then used by the calculate_sdp
+        function.
+
+        Parameters:
+        -----------
+
+            ambient_temp : float
+                ambient temperature to calculate the losses of the sdp
+
+            inlet_temp : float
+                temperature of the air at the inlet of the first sdp
+
+            absorption_incl : float
+                effective global irradiation on the sdp (optical losses already
+                included!)
+
+            mass_flow : float
+                mass flow of air at the point behind the fan. It will evenly be
+                devided by the model to each parallel string of sdps
+
+            zeta : float
+                the zeta value of the gap between the sdps to simulate the
+                "leakage" of air, or for this model the stream of air drawn in
+                between the sdps.
+
+            m_loss : float
+                the massflow of air at each "leakage" to initialize the zeta
+                value. This is to be taken with care and does not really work
+                yet, because the massflow cannot be the same for each sdp in
+                the series of sdps. It is an optional parameter - better not
+                use it for now.
+
+            print_res : boolean, default = True
+                if set True, it will print out the results of the simulation
+                Set this parameter to False to suppress this.
+
+        Returns:
+        --------
+
+            t_out : float
+                The temperature at the outlet, behind the fan.
+
+            p_fan : float
+                The power of the fan needed to provide the massflow.
+
+            m_out : float
+                The massflow at the outlet, behind the fan. It should be the
+                same, as what was given as mass_flow.
+        """
+
+        # network
+        fluid_list = ['air']
+        self.nw = Network(fluids=fluid_list,
+                          p_unit='bar',
+                          T_unit='C',
+                          v_unit='m3 / s',
+                          m_range=[0.0001, 11],
+                          p_range=[0.9, 1.02],
+                          h_range=[0, 100000],
+                          T_range=[-10, 100])
+
+        # %% components
+
+        # Sinks & Sources
+        feed_coll = Source('to collector')
+        from_coll = Sink('from collector')
+
+        # fan
+        fan = Compressor('fan')
+
+        mass_flow = mass_flow / self.num_sdp_parallel
+
+        # sdp_subsystem
+        sdp_sub = sdp_subsys('sdp',
+                             num_sdp_series=self.num_sdp_series,
+                             lkf_lin_sdp=self.loss_key_figures_lin,
+                             lkf_quad_sdp=self.loss_key_figures_quad,
+                             A_sdp=self.surface_area_sdp,
+                             Tamb=ambient_temp,
+                             E_sdp=absorption_incl,
+                             ks_sdp=self.ks,
+                             d_sdp=self.d_sdp,
+                             l_sdp=self.l_sdp,
+                             zeta_sdp=zeta,
+                             m_loss=m_loss,
+                             p_amb=p_amb)
+
+        # %% Connections
+
+        out_num = self.num_sdp_series - 1
+        feedc_sdp = Connection(feed_coll,
+                               'out1',
+                               sdp_sub.comps["sdp_0"],
+                               'in1')
+        sdp_fan = Connection(sdp_sub.comps["merge_" + str(out_num)],
+                             'out1',
+                             fan,
+                             'in1')
+        self.nw.add_conns(feedc_sdp, sdp_fan)
+
+        fan_fromc = Connection(fan, 'out1', from_coll, 'in1')
+        self.nw.add_conns(fan_fromc)
+
+        self.nw.add_subsys(sdp_sub)
+
+        fan.char_warnings = False
+        fan.set_attr(
+            eta_s=0.5,
+            design=['eta_s'],
+            offdesign=['eta_s_char']
+        )
+
+        # %% Connection parameters
+        feedc_sdp.set_attr(
+            m0=mass_flow,
+            T=inlet_temp,
+            fluid={'air': 1},
+            p=p_amb,
+        )
+
+        sdp_fan.set_attr(
+            p0=1,
+        )
+
+        fan_fromc.set_attr(
+            p=p_amb,
+            m=mass_flow,
+        )
+
+        # %% solving
+
+        mode = 'design'
+        self.nw.check_network()
+        # self.nw.save('sdp')
+        self.nw.solve(mode=mode)
+        self.nw.save('sdp')
+        if print_res:
+            self.nw.print_results()
+
+        # %% return characteristic parameters
+
+        t_out = fan_fromc.T.val_SI - Temp_Units[self.nw.T_unit][0]
+        p_fan = fan.P.val_SI * self.num_sdp_parallel
+        m_out = fan_fromc.m.val_SI * self.num_sdp_parallel
+        return t_out, p_fan, m_out
+
+    # %% calculation of the sdp
+
+    def calculate_sdp(self,
+                      ambient_temp,
+                      absorption_incl,
+                      inlet_temp,
+                      mass_flow,
+                      p_amb=1.01325,
+                      print_res=True):
+        """
+        TESPy calculation of the sdp after initialization.
+
+        Parameters:
+        -----------
+
+            ambient_temp : float
+                ambient temperature to calculate the losses of the sdp
+
+            inlet_temp : float
+                temperature of the air at the inlet of the first sdp
+
+            absorption_incl : float
+                effective global irradiation on the sdp (optical losses already
+                included!)
+
+            mass_flow : float
+                mass flow of air at the point behind the fan. It will evenly be
+                devided by the model to each parallel string of sdps
+
+        Returns:
+        --------
+
+            t_out : float
+                The temperature at the outlet, behind the fan.
+
+            p_fan : float
+                The power of the fan needed to provide the massflow.
+
+            m_out : float
+                The massflow at the outlet, behind the fan. It should be the
+                same, as what was given as mass_flow.
+        """
+
+        if ambient_temp is not None:
+            for comp in self.nw.comps['object']:
+                if isinstance(comp, SolarCollector):
+                    comp.set_attr(Tamb=ambient_temp)
+
+        if absorption_incl is not None:
+            for comp in self.nw.comps['object']:
+                if isinstance(comp, SolarCollector):
+                    comp.set_attr(E=absorption_incl)
+
+        if inlet_temp is not None:
+
+            for conn in self.nw.conns['object']:
+                a = type(conn)
+                if isinstance(conn.source, Source):
+                    conn.set_attr(T=inlet_temp)
+
+        if mass_flow is not None:
+            mass_flow = mass_flow / self.num_sdp_parallel
+            for conn in self.nw.conns['object']:
+                if isinstance(conn.source, Compressor):
+                    conn.set_attr(m=mass_flow)
+
+        self.nw.save("sdp")
+        # %% solving
+        mode = 'offdesign'
+        self.nw.solve(mode=mode, design_path='sdp')
+        if print_res:
+            self.nw.print_results()
+
+        # get parameters
+        for conn in self.nw.conns['object']:
+            if isinstance(conn.source, Compressor) & isinstance(conn.target, Sink):
+                t_out = conn.T.val_SI - Temp_Units[self.nw.T_unit][0]
+                m_out = conn.m.val_SI * self.num_sdp_parallel
+
+        for comp in self.nw.comps['object']:
+            if isinstance(comp, Compressor):
+                p_fan = comp.P.val * self.num_sdp_parallel
+
+        return t_out, p_fan, m_out
